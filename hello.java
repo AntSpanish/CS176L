1a90cd1431aea8cdcd778a9872cf5c6fd2816d47
AntSpanish <114097276+AntSpanish@users.noreply.github.com>
2023-09-07 16:09:11 -0400

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+public class hello 
+{
+
+	public static void main(String[] args)
+	{
+		//CS176L
+		
+		//Ant Spanish
+		
+		//helloWorld
+		System.out.println("Hello World");
+		
+	}
+	
+}
